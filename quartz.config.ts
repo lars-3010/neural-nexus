9a25448e5f8c1f58f8e320bc56fb04203a47f835
Lars Boes <iLars.Boes@gmail.com>
2024-10-12 15:22:16 +0200

    test

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lars-3010.github.io/neural-nexus",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
