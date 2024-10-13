8fef7ab03676250779375ed12db99e98e0485f71
Lars Boes <iLars.Boes@gmail.com>
2024-10-13 22:59:33 +0200

    page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Homepage",
+    pageTitle: "🪴 Neural Nexus",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
